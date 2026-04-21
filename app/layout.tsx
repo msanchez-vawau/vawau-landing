64dd23e9be95a6db6ae513b18ade2c992075cf1f
Marco Sanchez <msanchez@vawau.com>
2026-04-21 06:54:46 -0600

    add google analytics tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import { Montserrat, Saira_Stencil_One } from "next/font/google"
+import Script from "next/script"
 import "./globals.css"
 import WhatsAppFloat from "@/components/ui/WhatsAppFloat"
 
@@ -32,6 +33,21 @@ export default function RootLayout({
       <body>
         {children}
         <WhatsAppFloat />
+
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-JF8B99GGR0"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-JF8B99GGR0');
+          `}
+        </Script>
+
       </body>
     </html>
   )
