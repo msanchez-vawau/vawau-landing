19eacb8d0e06592490ee7912d5d762009528c6a2
Marco Sanchez <msanchez@vawau.com>
2026-04-21 07:06:51 -0600

    add conversion tracking base

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,8 +43,16 @@ export default function RootLayout({
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
+            window.gtag = gtag;
+
             gtag('js', new Date());
             gtag('config', 'G-JF8B99GGR0');
+
+            window.trackEvent = function(name){
+              gtag('event', name, {
+                event_category: 'engagement'
+              });
+            }
           `}
         </Script>
 
